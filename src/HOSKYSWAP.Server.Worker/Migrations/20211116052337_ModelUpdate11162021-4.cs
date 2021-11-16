80811a3a559a0bc83c79351780c35352a3fa097d
Clark Alesn <clark_alesna@hotmail.com>
2021-11-16 17:34:29 +0800

    OrderSync Implementation

---
+++ src/HOSKYSWAP.Server.Worker/Migrations/20211116052337_ModelUpdate11162021-4.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HOSKYSWAP.Server.Worker.Migrations
+{
+    public partial class ModelUpdate111620214 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<long>>(
+                name: "TxIndexes",
+                table: "Orders",
+                type: "bigint[]",
+                nullable: false,
+                oldClrType: typeof(List<int>),
+                oldType: "integer[]");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<int>>(
+                name: "TxIndexes",
+                table: "Orders",
+                type: "integer[]",
+                nullable: false,
+                oldClrType: typeof(List<long>),
+                oldType: "bigint[]");
+        }
+    }
+}
--- src/HOSKYSWAP.Server.Worker/Order.cs
+++ src/HOSKYSWAP.Server.Worker/Order.cs
@@ -5,6 +5,9 @@ public enum Status
     Open,
     Filled,
     Canceled,
+    Error,
+    Ignored,
+    Confirming
 }
 
 public record Order
@@ -12,9 +15,11 @@ public record Order
     public Guid Id { get; set; } = Guid.NewGuid();
     public string OwnerAddress { get; set; } = string.Empty;
     public string TxHash { get; set; } = string.Empty;
-    public int TxIndex { get; set; } = 0;
+    public List<long> TxIndexes { get; set; } = new();
     public string Action { get; set; } = string.Empty;
     public decimal Rate { get; set; } = 0;
     public ulong Total { get; set; } = 0;
     public Status Status { get; set; } = Status.Open;
+    public string ExecuteTxId { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
